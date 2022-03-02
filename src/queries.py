b565f6ce907a62c05dc8b9610689fb7d27b30074
Daniel La Rubia <daniel.rolim@b2wdigital.com>
2022-03-02 17:05:20 -0300

    list of queries

---
+++ src/queries.py
@@ -0,0 +1,25 @@
+# ---------- INSERT ---------- #
+
+insert_domain = "INSERT INTO domains (name, registrar_id, registrant_id, admin_id, tech_id, saci, name_servers, creation_date, expiration_date, updated_date) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT DO NOTHING"
+
+insert_admin = "INSERT INTO admin (name, rit, organization, street, city, state_province, postal_code, country, phone, email, application_purpose, nexus_category, created, changed) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+
+insert_registrant = "INSERT INTO registrant (name, rit, organization, street, city, state_province, postal_code, country, phone, email, application_purpose, nexus_category) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+
+insert_registrar = "INSERT INTO registrar (name, rid, url, whois_url, email, phone) VALUES (%s, %s, %s, %s, %s, %s)"
+
+insert_tech = "INSERT INTO tech (name, rit, organization, street, city, state_province, postal_code, country, phone, email, application_purpose, nexus_category, created, changed) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+
+
+# ---------- SELECT ---------- #
+select_all_domains = "SELECT * FROM domains"
+
+select_domain_where_name_equals_to = "SELECT * FROM domains WHERE name = %s"
+
+select_registrar_id_where_name_equals_to = "SELECT id FROM registrars WHERE name = %s"
+
+select_registrant_id_where_name_equals_to = "SELECT id FROM registrants WHERE name = %s"
+
+select_admin_id_where_name_equals_to = "SELECT id FROM admins WHERE name = %s"
+
+select_tech_id_where_name_equals_to = "SELECT id FROM tech WHERE name = %s"
