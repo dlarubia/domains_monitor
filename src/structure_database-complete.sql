8416e6746e76383297f572c9ecd1e86ac5a4fc66
Daniel La Rubia <daniel.rolim@b2wdigital.com>
2022-03-04 16:09:25 -0300

    improvements to database structure

--- src/structure_database-complete.sql
+++ src/structure_database-complete.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS registrars (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
---  rid VARCHAR(100),
+    name VARCHAR(255) UNIQUE NOT NULL,
+    rid VARCHAR(100),
     url VARCHAR(255),
     whois_url VARCHAR(255),
     email VARCHAR(255),
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS registrars (
 
 CREATE TABLE IF NOT EXISTS registrants (
     id SERIAL PRIMARY KEY, 
-    name VARCHAR(255),
+    name VARCHAR(255) UNIQUE NOT NULL,
     rid VARCHAR(100), -- CPF ou CPNJ ou Código estrangeiro
     organization VARCHAR(255),
     street VARCHAR(255),
@@ -25,9 +25,9 @@ CREATE TABLE IF NOT EXISTS registrants (
     );
     
 CREATE TABLE IF NOT EXISTS admins (
-    id SERIAL PRIMARY KEY, 
-    name VARCHAR(255),
-    rid VARCHAR(100), -- CPF ou CPNJ ou Código estrangeiro
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    aid VARCHAR(100), 
     organization VARCHAR(255),
     street VARCHAR(255),
     city VARCHAR(255),
@@ -37,16 +37,16 @@ CREATE TABLE IF NOT EXISTS admins (
     phone VARCHAR(255),
     email VARCHAR(255),
     application_purpose VARCHAR(255),
-    nexus_category VARCHAR(255),
-    created VARCHAR(100),
-    changed VARCHAR(100)
+    nexus_category VARCHAR(255)
+    -- created VARCHAR(100),
+    -- changed VARCHAR(100)
     );
 
     
 CREATE TABLE IF NOT EXISTS techs (
     id SERIAL PRIMARY KEY, 
-    name VARCHAR(255),
-    rid VARCHAR(100), -- CPF ou CPNJ ou Código estrangeiro
+    name VARCHAR(255) UNIQUE NOT NULL,
+    tid VARCHAR(100), -- CPF ou CPNJ ou Código estrangeiro
     organization VARCHAR(255),
     street VARCHAR(255),
     city VARCHAR(255),
@@ -56,15 +56,16 @@ CREATE TABLE IF NOT EXISTS techs (
     phone VARCHAR(255),
     email VARCHAR(255),
     application_purpose VARCHAR(255),
-    nexus_category VARCHAR(255),
-    created VARCHAR(100),
-    changed VARCHAR(100)
+    nexus_category VARCHAR(255)
+    -- created VARCHAR(100),
+    -- changed VARCHAR(100)
     );
    
         
 CREATE TABLE IF NOT EXISTS domains (
-    id SERIAL,
-    name VARCHAR(255) PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    did VARCHAR(50),
     
     registrar_id INT,
         FOREIGN KEY (registrar_id)
@@ -79,12 +80,13 @@ CREATE TABLE IF NOT EXISTS domains (
         FOREIGN KEY (tech_id)
             REFERENCES techs(id),
     
-    saci BOOLEAN,
+    saci VARCHAR(10),
     name_servers TEXT,
     creation_date VARCHAR(255),
     expiration_date VARCHAR(255),
-    updated_date TEXT --changed
-    status TEXT
+    updated_date TEXT, --changed
+    status TEXT,
+    email TEXT
 );
 
 
@@ -94,6 +96,7 @@ CREATE TABLE IF NOT EXISTS domains_history (
     operation VARCHAR(20),
 
     name VARCHAR(255),
+    did VARCHAR(50),
     
     registrar_id INT,
         FOREIGN KEY (registrar_id)
@@ -108,22 +111,24 @@ CREATE TABLE IF NOT EXISTS domains_history (
         FOREIGN KEY (tech_id)
             REFERENCES techs(id),
     
-    saci BOOLEAN,
+    saci VARCHAR(10),
     name_servers TEXT,
     creation_date VARCHAR(255),
     expiration_date VARCHAR(255),
-    updated_date TEXT --changed
+    updated_date TEXT, --changed
+    status TEXT,
+    email TEXT
 );
 
 CREATE OR REPLACE FUNCTION copy_domain_info() RETURNS trigger AS 
     $$
         BEGIN
             INSERT INTO domains_history
-                (operation, id, name, registrar_id, registrant_id, admin_id, tech_id, saci,
-                name_servers, creation_date, expiration_date, updated_date)
+                (operation, name, did, registrar_id, registrant_id, admin_id, tech_id, saci,
+                name_servers, creation_date, expiration_date, updated_date, status, email)
             VALUES
-                ('UPDATE', OLD.id, OLD.name, OLD.registrar_id, OLD.registrant_id, OLD.admin_id, OLD.tech_id, OLD.saci,
-                OLD.name_servers, OLD.creation_date, OLD.expiration_date, OLD.updated_date);
+                ('UPDATE', OLD.name, OLD.id, OLD.registrar_id, OLD.registrant_id, OLD.admin_id, OLD.tech_id, OLD.saci,
+                OLD.name_servers, OLD.creation_date, OLD.expiration_date, OLD.updated_date, OLD.status, OLD.email);
             RETURN OLD;
         END;
     $$
