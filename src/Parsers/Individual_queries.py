af95267419fc47ce478b3a7faf39672b64168c64
Daniel La Rubia <daniel.rolim@b2wdigital.com>
2022-03-17 12:42:58 -0300

    new queries and minor changes

--- src/Parsers/Individual_queries.py
+++ src/Parsers/Individual_queries.py
@@ -11,18 +11,18 @@ GLOBAL FIELDS
 ########## GoDaddy ##########
 
 # ---------- INSERT ---------- #
-go_daddy_insert_domain = "INSERT INTO domains (name, did, name_servers, creation_date, expiration_date, updated_date, status) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+go_daddy_insert_domain = "INSERT INTO domains (name, did, name_servers, creation_date, expiration_date, updated_date, status) VALUES (%s, %s, %s, %s , %s, %s, %s) ON CONFLICT DO NOTHING"
 
-go_daddy_insert_registrar = "INSERT INTO registrars (name, rid, url, whois_url, email, phone) VALUES (%s, %s, %s, %s, %s, %s)"
+go_daddy_insert_registrar = "INSERT INTO registrars (name, rid, url, whois_url, email, phone) VALUES (%s, %s, %s, %s, %s, %s) ON CONFLICT DO NOTHING"
 
-go_daddy_insert_registrant = "INSERT INTO registrants (name, rid, organization, street, city, state_province, postal_code, country, phone, email, application_purpose, nexus_category) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+go_daddy_insert_registrant = "INSERT INTO registrants (name, rid, organization, street, city, state_province, postal_code, country, phone, email, application_purpose, nexus_category) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT DO NOTHING"
 
-go_daddy_insert_admin = "INSERT INTO admins (name, aid, organization, street, city, state_province, postal_code, country, phone, email, application_purpose, nexus_category) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+go_daddy_insert_admin = "INSERT INTO admins (name, aid, organization, street, city, state_province, postal_code, country, phone, email, application_purpose, nexus_category) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT DO NOTHING"
 
-go_daddy_insert_tech = "INSERT INTO techs (name, tid, organization, street, city, state_province, postal_code, country, phone, email, application_purpose, nexus_category) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+go_daddy_insert_tech = "INSERT INTO techs (name, tid, organization, street, city, state_province, postal_code, country, phone, email, application_purpose, nexus_category) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT DO NOTHING"
 
 # ---------- UPDATE ---------- #
-go_daddy_update_registrar_id_in_domain = 'UPDATE IN domains (registrar_id, registrant_id, admin_id, tech_id) VALUES (%s, %s, %s, %s)'
+go_daddy_update_registrar_id_in_domain = 'UPDATE IN domains (registrar_id, registrant_id, admin_id, tech_id) VALUES (%s, %s, %s, %s) ON CONFLICT DO NOTHING'
 
 
 # ---------- DELETE ---------- #
@@ -31,21 +31,21 @@ go_daddy_update_registrar_id_in_domain = 'UPDATE IN domains (registrar_id, regis
 ########## RegistroBR ##########
 
 # ---------- INSERT ---------- #
-registrobr_insert_domain = "INSERT INTO domains (name, saci, name_servers, creation_date, expiration_date, updated_date, status, email) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+registrobr_insert_domain = "INSERT INTO domains (name, saci, name_servers, creation_date, expiration_date, updated_date, status, email) VALUES (%s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT DO NOTHING"
 
-registrobr_insert_registrant = "INSERT INTO registrants (name, rid) VALUES (%s, %s)"
+registrobr_insert_registrant = "INSERT INTO registrants (name, rid) VALUES (%s, %s) ON CONFLICT DO NOTHING"
 
 # I'll put owner into admins
-registrobr_insert_admin = "INSERT INTO admins (name) VALUES (%s)"
+registrobr_insert_admin = "INSERT INTO admins (name) VALUES (%s) ON CONFLICT DO NOTHING"
 
-registrobr_insert_tech = "INSERT INTO techs (name) VALUES (%s)"
+registrobr_insert_tech = "INSERT INTO techs (name) VALUES (%s) ON CONFLICT DO NOTHING"
 
 
 
 ########## MarkMonitor ##########
 
 # ---------- INSERT ---------- #
-markmonitor_insert_domain = "INSERT INTO domains (name, name_servers, creation_date, expiration_date) VALUES (%s, %s, %s, %s)"
+markmonitor_insert_domain = "INSERT INTO domains (name, name_servers, creation_date, expiration_date) VALUES (%s, %s, %s, %s) ON CONFLICT DO NOTHING"
 
 markmonitor_insert_registrant = "INSERT INTO registrants (name, organization) VALUES (%s, %s) ON CONFLICT DO NOTHING"
 
@@ -54,11 +54,15 @@ markmonitor_insert_registrar = "INSERT INTO registrars (name, url) VALUES (%s, %
 
 
 ########## Global Queries ##########
-update_domain_keys = "UPDATE domains SET registrar_id = %s, registrant_id = %s, admin_id = %s, tech_id = %s WHERE name = %s"
+update_domain_keys = "UPDATE domains SET provider_id = %s, registrar_id = %s, registrant_id = %s, admin_id = %s, tech_id = %s WHERE name = %s"
 
-insert_invalid_domain = "INSERT INTO domains (name) VALUES (%s)"
+insert_provider = "INSERT INTO providers (name) VALUES (%s) ON CONFLICT DO NOTHING"
 
-select_registrar_id_by_name = "SELECT id FROM registrars WHERE name = %s"
+insert_invalid_domain = "INSERT INTO domains (name, provider_id) VALUES (%s, %s) ON CONFLICT DO NOTHING"
+
+select_provider_id_by_name = "SELECT id FROM providers WHERE name = (%s)"
+
+select_registrar_id_by_name = "SELECT id FROM registrars WHERE name = (%s)"
 
 select_registrant_id_by_name = "SELECT id FROM registrants WHERE name = %s"
 
@@ -66,6 +70,7 @@ select_admin_id_by_name = "SELECT id FROM admins WHERE name = %s"
 
 select_tech_id_by_name = "SELECT id FROM techs WHERE name = %s"
 
+select_domains_with_provider = "SELECT domains.name, providers.name FROM domains INNER JOIN providers ON provider_id = providers.id"
 
 ## -------- GLOBAL DICTIONARY -------- #
 # TODO -> Review registrars names
@@ -93,10 +98,13 @@ queries = {
 
     "Global" : {
         "update_domain_keys" : update_domain_keys,
+        "insert_provider" : insert_provider,
         "insert_invalid_domain" : insert_invalid_domain,
+        "select_provider_id_by_name" : select_provider_id_by_name,
         "select_registrar_id_by_name" : select_registrar_id_by_name,
         "select_registrant_id_by_name" : select_registrant_id_by_name,
         "select_admin_id_by_name" : select_admin_id_by_name,
-        "select_tech_id_by_name" : select_tech_id_by_name
+        "select_tech_id_by_name" : select_tech_id_by_name,
+        "select_domains_with_provider" : select_domains_with_provider
     }
 }
