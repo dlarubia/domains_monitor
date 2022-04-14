9fa673583122e3731be6202bdc91cc505c8a838e
Daniel La Rubia <daniel.rolim@b2wdigital.com>
2022-04-14 11:28:01 -0300

    default routine

--- main.py
+++ main.py
@@ -5,12 +5,18 @@
 from src.Checker import Checker
 from src.Database import Database
 from src.config.credentials import database as db
+import os
+# from src.Menu import menu
 
-db = Database(user=db['user'], password=db['password'], host=db['host'], port=db['port'], db_name=db['name'], sql_script = db['sql_script'])
-ch = Checker(database=db, first_time=True)
-ch.whois()
-ch.insert_domains_in_database()
-# ch.export_domains_info_as_csv()
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# menu()
+db = Database(user=db['user'], password=db['password'], host=db['host'], port=db['port'], db_name=db['name'], sql_script = ROOT_DIR + db['sql_script'])
+ch = Checker(database=db, first_time=True, domains_folder=ROOT_DIR+'/domains')
+# # ch.whois()
+# # ch.insert_domains_in_database()
+# # ch.export_domains_info_as_csv()
+ch.check_all_domains_and_update_db('domains')
 
 print('done')
 pass
